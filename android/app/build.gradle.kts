7895a4c834c5c01ec87004fa1f75579832412cf7
F0rtyDegree <72554040+F0rtyDegree@users.noreply.github.com>
2026-05-13 14:07:09 +0000

    ver. 3.6.0 подпись сертификатом FortyDegree

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,21 +19,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "by.fortydegree.compass40"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    // Определяем наш релизный signingConfig
+    signingConfigs {
+        create("release") {
+            storeFile = file("upload-keystore.jks")
+            storePassword = "Compass40"
+            keyAlias = "upload"
+            keyPassword = "Compass40"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            // Вот она — главная правка: используем наш конфиг, а не debug
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
