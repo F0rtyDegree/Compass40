a0e9f0a3b705fd7ec88d1e43479cc1a81e037623
F0rtyDegree <72554040+F0rtyDegree@users.noreply.github.com>
2026-07-09 13:31:15 +0000

    minor 1

--- lib/models/geo_point.dart
+++ lib/models/geo_point.dart
@@ -3,9 +3,3 @@ class GeoPoint {
   final double longitude;
   const GeoPoint({required this.latitude, required this.longitude});
 }
-
-class LocalPoint {
-  final double east;
-  final double north;
-  const LocalPoint({required this.east, required this.north});
-}
